9be42305ab6c8da4ac079ebf0dff57d01ab7db53
Tanxao <143907080+Tanxao@users.noreply.github.com>
2023-12-24 22:11:32 +0200

    use

--- js/task-4.js
+++ js/task-4.js
@@ -1,12 +1,12 @@
 `use strict`
-const getTotalBalanceByGender = (users, gender) => {
-  return users
+
+const getTotalBalanceByGender = (users, gender) =>
+  users
     .filter(user => user.gender === gender)
     .reduce((totalBalance, user) => totalBalance + user.balance, 0);
-};
 
 const allUsers = [
-    {
+  {
     name: "Moore Hensley",
     gender: "male",
     balance: 2811
